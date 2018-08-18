8fb7403c565d2560d00a6ea2d17cc1baeb8671ab
Tucker <takahirosuzuki.m010@gmail.com>
2018-08-18 09:44:46 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Jokes  
+  
+This project is Angular 6 based  PWA (Progressive Web Apps), followed the [coursetro Building Your First Angular PWA from Scratch (Angular 6)](https://coursetro.com/posts/code/167/Building-Your-First-Angular-PWA-from-Scratch-(Angular-6)-) tutorial by [Gary Simon](http://www.garysimon.com/) and was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.3.  
+  
+  
+# Reference  
+  
+[chucknorris.io](https://api.chucknorris.io/)  
+[Angular Official httpClientModule](https://angular.io/guide/http)  
+  
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'jokes';
+
+  update: boolean = false;
+  joke: any;
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+      //this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
+    })
+  }
+  
+  ngOnInit(){
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res;
+    })
+  }
+
+}
